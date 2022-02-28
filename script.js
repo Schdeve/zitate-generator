602dce1743f11c2a6bcbe440adfda26f14f188ae
stefan.keller <skeller2@gmx.net>
2022-02-28 14:28:10 +0100

    update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function generiereZitate() {
     var zitate = {
-        "Albers, Steffen": "Die Realität verschwindet nicht, nur weil man seine Augen vor ihr verschließt.",
+        "Albers Steffen": "Die Realität verschwindet nicht, nur weil man seine Augen vor ihr verschließt.",
         "Paulick Harald": "Wer andere Menschen hasst, hat den Boden der Demokratie verlassen." ,
         "Lippold Steve": "Wer sich im Genuss übt, übersättigt sein Verlangen nach Gierheit.",
         "Kühn-Görg Monika": "Unsere höchste Mission ist es, die Erde so zu gestalten, um sie für nächste Generationen zu erhalten.",
@@ -9,9 +9,12 @@ function generiereZitate() {
         "Seibold Klaus": "Es macht wenig Sinn, sich Sorgen über die Zukunft zu machen. Es gilt erstmal, den heutigen Tag zu überstehen.",
     }
 
-    var autoren = Object.keys(zitate);
+    var autoren = [
+        "Albers Steffen", "Paulick Harald", "Lippold Steve", "Kühn-Görg Monika",
+        "Ockenga Klaas", "Vagianos Athanasios", "Seibold Klaus",
+    ] 
+    
     var autor = autoren[Math.floor(Math.random() * autoren.length)];
-
     var zitat = zitate[autor];
 
     document.getElementById("zitate").innerHTML = zitat;
